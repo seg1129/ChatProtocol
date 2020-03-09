736257a6b160ec0cd908354b8df1f19e7902c9b0
Suzanne Gerace <SEG1129@gmail.com>
2020-03-08 22:09:48 -0400

    used to test server

---
+++ server/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+# Suzanne Gerace
+# test server file
+
+import socket
+import time
+import logging
+
+logging.basicConfig(filename='server_test.log', level=logging.DEBUG)
+# a socket object using IPv4
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# get local machine name
+host = socket.gethostname()
+
+port = 5035
+
+# connection to hostname on the port.
+s.connect(('localhost', port))
+
+time.sleep(5)
+print("connected to server")
+# 1024 bytes is the maximum
+msg = s.recv(1024)
+print (msg.decode('ascii'))
+# send username
+message = 'USERPamina'
+s.send(message.encode('utf-8'))
+msg2 = s.recv(1024)
+print (msg2.decode('ascii'))
+time.sleep(5)
+# send password
+password_command = 'PASSILovePittbulls'
+s.send(password_command.encode('utf-8'))
+pass_response = s.recv(1024)
+print (pass_response.decode('ascii'))
+
+rmsg_command = 'RMSG'
+s.send(rmsg_command.encode('utf-8'))
+rmsg_response = s.recv(1024)
+print (rmsg_response.decode('ascii'))
+
+smsg = 'SMSG Dolce:want to go to the doggy park Dolce?'
+s.send(smsg.encode('utf-8'))
+smsg_response = s.recv(1024)
+print(smsg_response.decode('ascii'))
+
+bad_password_command = 'PASSILovePittbulls'
+s.send(bad_password_command.encode('utf-8'))
+bad_pass_response = s.recv(1024)
+print (bad_pass_response.decode('ascii'))
+time.sleep(5)
+s.close()
